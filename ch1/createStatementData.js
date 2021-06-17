3cfa8a5015a482e20b22b4a412159b6627c82d41
redpandaathome <leeyumi415@gmail.com>
2021-06-17 18:08:11 +0900

    p72 subclass

--- ch1/createStatementData.js
+++ ch1/createStatementData.js
@@ -1,24 +1,28 @@
 function createPerfrmanceCalculator(aPerformance, aPlay) {
-  return new PerformanceCalculator(aPerformance, aPlay)
+  switch (aPlay.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+  }
 }
+
 class PerformanceCalculator {
   constructor(aPerformance, aPlay) {
     this.performance = aPerformance;
-    this.play = aPlay
+    this.play = aPlay;
   }
 
   //copied from function amountFor()
-  get amount(){
-    console.log("this.performance: ",this.performance)
-    console.log("this.play: ",this.play)
-
+  get amount() {
+    console.log("this.performance: ", this.performance);
+    console.log("this.play: ", this.play);
     let result = 0;
-    switch (this.performance.play.type) {
+    switch (this.play.type) {
       case "tragedy":
-        result = 40000;
-        if (this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30);
-        }
+        throw '오류 발생!' //비극 공연료는 TragedyCalculator를 이용하도록 유도
         break;
       case "comedy":
         result = 30000;
@@ -33,7 +37,7 @@ class PerformanceCalculator {
     return result;
   }
 
-  get volumeCredits(){
+  get volumeCredits() {
     let result = 0;
     // q1. 초반에 [0] 부분
     // q2.어째서 this.performance와 this.play를 혼용해서 쓸까? 이미 전자에 필요한 정보가 다 있는데?
@@ -44,7 +48,21 @@ class PerformanceCalculator {
   }
 }
 
-export default function createStatementData(invoice, plays){
+class TragedyCalculator extends PerformanceCalculator {
+  get amount(){
+    let result = 40000;
+      if (this.performance.audience > 30) {
+        result += 1000 * (this.performance.audience - 30);
+      }
+      return result;
+  }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+  
+}
+
+export default function createStatementData(invoice, plays) {
   const result = {};
   result.customer = invoice[0].customer;
   result.performances = invoice[0].performances.map(enrichPerformance);
@@ -52,33 +70,38 @@ export default function createStatementData(invoice, plays){
   result.totalVolumeCredits = totalVolumeCredits(result);
   return result;
 
-  function enrichPerformance(aPerformance){
+  function enrichPerformance(aPerformance) {
     //공연료 계산기 생성, 공연 정보를 계산기로 전달
-    const calculator = new createPerfrmanceCalculator(aPerformance, playFor(aPerformance));
-    console.log("aPerf", aPerformance)
-    const result = Object.assign({}, aPerformance)
+    const calculator = new createPerfrmanceCalculator(
+      aPerformance,
+      playFor(aPerformance)
+    );
+    console.log("aPerf", aPerformance);
+    const result = Object.assign({}, aPerformance);
     result.play = calculator.play;
     result.amount = calculator.amount;
     result.volumeCredits = calculator.volumeCredits;
-    console.log("result: ", result)
+    console.log("result: ", result);
     return result;
   }
 
-  function playFor(aPerformance){
-    return plays[aPerformance.playId]
+  function playFor(aPerformance) {
+    return plays[aPerformance.playId];
   }
 
   function amountFor(aPerformance) {
-    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance))
+      .amount;
   }
 
-  function totalAmount(data){
+  function totalAmount(data) {
     //p58. 반복문을 파이프라인으로 바꾸기
-    return data.performances.reduce((total, p)=>{total+=p.amount, 0})
+    return data.performances.reduce((total, p) => {
+      (total += p.amount), 0;
+    });
   }
 
-  function totalVolumeCredits(data){
-    return data.performances.reduce((total, p)=> total+p.volumeCredits ,0)
- }
+  function totalVolumeCredits(data) {
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
+  }
 }
-
