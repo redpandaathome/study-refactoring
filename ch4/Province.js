3e8df2d1a55206397d709793e7c30dd5f99ac886
redpandaathome <leeyumi415@gmail.com>
2021-08-04 08:51:35 +0900

    wip

---
+++ ch4/Province.js
@@ -0,0 +1,28 @@
+class Province {
+   constructor(doc) {
+      this._name = doc.name;
+      this._producers = [];
+      this._totalProduction = 0;
+      this._demand = doc.demand;
+      this._price = doc.price;
+      doc.producers.forEach(d=>this.addProducer(new Producer(this, d)))
+   }
+   
+   addProducer(arg) {
+      this._producers.push(arg);
+      this._totalProduction += arg.production;
+   }
+}
+
+function sampleProvinceData(){
+   return {
+      name: "Asia",
+      producers: [
+         {name: "Byzantium", cost:10, production:9},
+         {name: "Attalia", cost:12, production:10},
+         {name: "Synope", cost:10, production:6},
+      ],
+      demand: 30,
+      price: 20
+   }
+}
