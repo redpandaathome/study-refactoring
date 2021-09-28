68dd60a929dd8bbbf72faba287bc9e71ddc67713
redpandaathome <leeyumi415@gmail.com>
2021-09-28 17:32:29 +0900

    first test

---
+++ ch4/test/test.js
@@ -0,0 +1,12 @@
+// let assert = require('assert');
+import * as assert from 'assert'
+import 'mocha'
+import { Province, sampleProvinceData } from '../Province.js';
+describe('TEST START', function() {
+  describe('province', function() {
+    it('shortfall', function() {
+       const asia = new Province(sampleProvinceData());
+      assert.equal(asia.shortfall, 5);
+    });
+  });
+});
